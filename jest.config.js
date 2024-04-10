06270cb8008329a8a74a1cbc789ee30a921ba7ed
Kelvya Thais <kelvya.kt@gmail.com>
2024-04-10 10:15:17 -0300

    Arquivo de configurações jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  reporters: [
+    'default',
+    [
+      'jest-qase-reporter',
+      {
+        // Usando variável de ambiente para a API Token
+        apiToken: process.env.QASE_API_TOKEN,
+        // Usando variável de ambiente para o Project Code
+        projectCode: process.env.PROJECT_CODE,
+        // O runId pode ser dinâmico ou fixo, dependendo da necessidade
+        // runId: 45,
+        // environmentId deve ser configurado conforme necessário para o projeto
+        // environmentId: 1,
+        logging: true,
+        runComplete: true,
+      },
+    ],
+  ],
+};
